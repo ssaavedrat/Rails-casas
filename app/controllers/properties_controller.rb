3bde405a9c38d3b21e2cbd7d5e330005164d7393
Brayan Diaz C <brayandiazc@gmail.com>
2023-08-17 20:36:30 -0500

    Solucion a relaciones muchos a muchos controlador

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -66,6 +66,6 @@ class PropertiesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def property_params
-      params.require(:property).permit(:name, :description, :category_id, features_ids: [])
+      params.require(:property).permit(:name, :description, :category_id, { feature_ids: [] })
     end
 end
